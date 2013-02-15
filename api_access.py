b2830e7b62b835220e253da06d9e563a8a0485b4
jasminegao <jasminegao93@gmail.com>
2013-02-15 14:28:46 -0500

    added image checker

--- api_access.py
+++ api_access.py
@@ -1,16 +1,30 @@
 import urllib2
 import ujson as json
+import pprint
 
-ACCESS_TOKEN=open('API_KEY.no').read()
+ACCESS_TOKEN='53a01f38b09c0463cb9e2b35b151beb127843bf3'
+#ACCESS_TOKEN=open('API_KEY.no').read()
 API_URL='https://api-ssl.bitly.com'
 
 HIGH_VALUE_API = '/v3/highvalue?access_token='+ ACCESS_TOKEN +'&limit='
 LINK_INFO_API = '/v3/link/info?access_token='+ ACCESS_TOKEN +'&link='
 
-
+#get high value links
 limit = 10
 url = API_URL + HIGH_VALUE_API + str(limit)
 request = urllib2.urlopen(url)
 data = json.loads(request.read())
 
 return_urls = data['data']['values']
+
+#create an empty list to hold the image links
+image_links = []
+
+#for each high value link, return the link info
+for link in return_urls:
+    url = API_URL + LINK_INFO_API + link
+    request = urllib2.urlopen(url)
+    data = json.loads(request.read())
+    #check to see if link is an image, if so then we add it to image_links
+    if data['data']['category'] == 'image':
+        image_links.append(link)
