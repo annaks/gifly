ce30ad78e6addd500f3270c4de7e8bd0c7665b56
Anna Smith <anna@bit.ly>
2013-02-15 11:05:38 -0500

    fix bugs

--- api_access.py
+++ api_access.py
@@ -1,10 +1,10 @@
 import urllib2
 import ujson as json
 
-API_KEY=open('API_KEY.no').read()
+ACCESS_TOKEN=open('API_KEY.no').read()
 API_URL='https://api-ssl.bitly.com'
 
-HIGH_VALUE_API = '/v3/highvalue?access_token='+ API_KEY +'&limit='
+HIGH_VALUE_API = '/v3/highvalue?access_token='+ ACCESS_TOKEN +'&limit='
 LINK_INFO_API = '/v3/link/info?access_token='+ ACCESS_TOKEN +'&link='
 
 
@@ -13,4 +13,4 @@ url = API_URL + HIGH_VALUE_API + str(limit)
 request = urllib2.urlopen(url)
 data = json.loads(request.read())
 
-return_urls = data['values']
+return_urls = data['data']['values']
